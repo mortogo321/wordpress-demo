b50540cdb389db13b2046de04bc7080a13a5c0ce
Sekseason <sekseason@gmail.com>
2018-08-11 01:39:03 +0700

    index

--- wp-content/themes/demo/index.php
+++ wp-content/themes/demo/index.php
@@ -1,20 +1,23 @@
+<?php get_header();?>
+
+<main>
+  <?php get_template_part('partials/menu');?>
+
 <?php
-
-get_header();
-
 if (have_posts()):
     while (have_posts()): the_post();?>
 
-		        <h2><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
-				<?php the_content()?>
+				        <h2><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
+						<?php the_content()?>
 
-			<?php endwhile;
+					<?php endwhile;
 
 else:
     echo '<p>There are no posts!</p>';
 
 endif;
-
-get_footer();
-
 ?>
+
+</main>
+
+<?php get_footer();?>
